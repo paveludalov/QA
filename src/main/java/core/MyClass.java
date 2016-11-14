dc854161025be753977ca06d719ffe0775566281
Pavel Udalov <pavel.sf.udalov@gmail.com>
2016-11-13 21:10:46 -0800

    First commit

---
+++ src/main/java/core/MyClass.java
@@ -0,0 +1,18 @@
+package core;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	String name = "Pavel";
+	System.out.println("My name is "  + name );
+
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
